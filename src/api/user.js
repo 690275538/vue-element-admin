982942ec8b8cfb887012cf5581942ac1b8cf37ce
wangsen <690275538@qq.com>
2020-04-13 23:02:46 +0800

    增加 登录

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-element-admin/user/login',
+    url: '/SystemUser/login',
     method: 'post',
     data
   })
@@ -20,5 +20,12 @@ export function logout() {
   return request({
     url: '/vue-element-admin/user/logout',
     method: 'post'
+  })
+}
+
+export function test() {
+  return request({
+    url: '/SystemUser/test',
+    method: 'get'
   })
 }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -33,7 +33,14 @@ const actions = {
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
+      // test().then(response => {
+      // console.log(response)
+      // })
+      // return
+      login({
+        Account: username.trim(),
+        Password: password
+      }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
